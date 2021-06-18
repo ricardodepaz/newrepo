b4697e0458d05144269d81dd1ca87a5e10c67d15
dn192246 <ricar.arturo@gmail.com>
2021-06-18 12:45:02 -0600

    Creé el formulario y agregué una funcionalidad al mismo

---
+++ src/testproject2b/TestProject2B.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testproject2b;
+
+/**
+ *
+ * @author HP
+ */
+public class TestProject2B {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    public void saludar(){
+        System.out.println("Hola");
+    }
+}
